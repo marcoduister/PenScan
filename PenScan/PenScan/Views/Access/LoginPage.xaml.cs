dd7478eb95883c558efa776e9d4ff880e64a73a7
Prism098 <vandenbergmax@Outlook.com>
2022-01-24 22:39:27 +0100

    Added shake detect to login and reigstration page

--- PenScan/PenScan/Views/Access/LoginPage.xaml.cs
+++ PenScan/PenScan/Views/Access/LoginPage.xaml.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Xamarin.Essentials;
+
 namespace PenScan.Views.Access
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -17,6 +19,19 @@ namespace PenScan.Views.Access
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
+
+            Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+
+            if (!Accelerometer.IsMonitoring)
+                Accelerometer.Start(SensorSpeed.Game);
+        }
+
+        void Accelerometer_ShakeDetected(object sender, EventArgs e)
+        {
+            // Process shake event
+            Email.Text = "";
+            Password.Text = "";
+            DisplayAlert("Shake It", "Earthquake Detected", "Close");
         }
     }
 }
